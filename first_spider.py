e4b1177d7acc79bb1bed09e70d87602f0905b950
marc.ponce <marc.ponce@applus.com>
2024-05-09 12:41:00 +0200

    Works

--- first_spider.py
+++ first_spider.py
@@ -42,13 +42,13 @@ def get_moduleNames(s):
     soup = bs4(r.text, "html.parser")
     modules = soup.find_all("div", class_="card")
     module_names = {}
-    for module in modules[0:2]:
+    for module in modules[0:7]:
         module_name = module.find("img", alt=True)["alt"]
         module_names[module_name] = []
     return modules, module_names
 def get_moduleLinks(modules, headers):
     hrefs = []
-    for module in modules[:2]:
+    for module in modules[:7]:
         link = module.find("a")
         if link:
             hrefs.append(link.get("href"))
@@ -63,31 +63,37 @@ def get_moduleLinks(modules, headers):
         for r in redirect:
             a = r.get("href")
             if "/section/" in a:
-                print(a)
                 redirect_info.append(a)
         #os.system("clear")
     #better presentation as a list of lists
-    redirects = sorted(set(redirect_info))
-
+    #redirects = sorted(set(redirect_info))
     module_links = {}
     for link in redirect_info:
-        print(link.split("/"))
         module_number = link.split("/")[-3]
         # Append response content to the appropriate list based on the module number
         if module_number not in module_links:
             module_links[module_number] = []
         module_links[module_number].append(link)
-    return module_links
+
+    sorted_dict = {}
+
+    for key, value in module_links.items():
+        # Remove duplicates and sort based on the last number after a "/"
+        sorted_values = sorted(set(value), key=lambda x: int(re.search(r'/(\d+)$', x).group(1)))
+        sorted_dict[key] = sorted_values
+    return sorted_dict
 def get_content(module_links):
     content = {}
     os.system("clear")
-    for key, value_list in tqdm(module_links.items(), desc="Getting contents for each module"):
-        content[key] = []
-        for value in value_list:      
-            r = requests.get(value, headers=headers)
-            s = bs4(r.text,"html.parser")
-            moduletext= s.find("div", class_="training-module")
-            content[key].append(moduletext)
+    with tqdm(total=sum(len(value_list) for value_list in module_links.values()), desc="Getting contents for each module") as pbar1:
+        for key, value_list in module_links.items():
+            content[key] = []
+            for value in value_list:
+                r = requests.get(value, headers=headers)
+                s = bs4(r.text, "html.parser")
+                moduletext = s.find("div", class_="training-module")
+                content[key].append(moduletext)
+                pbar1.update(1)
     # put the contents into the dictionary with the key as the module name 
     for i, (content, value1) in enumerate(content.items()):
         if i < len(module_names):
@@ -97,19 +103,17 @@ def get_content(module_links):
 def to_markdown(module_names):
     os.system('clear')
     # Initialize the outer tqdm progress bar for module names
-    with tqdm(total=len(module_names.items()), desc="\rProcessing data for each module:") as outer_bar:
+    with tqdm(total=len(module_names.items()), desc="Processing data for each module:") as outer_bar:
         # Iterate over module names
         for key, value_list in module_names.items():
             # Initialize the inner tqdm progress bar for each value list
-            with tqdm(total=len(value_list), desc=f"\r{key}") as inner_bar:
                 # Iterate over values in the list
-                for i, text in enumerate(value_list):
-                    html_to_md = md(str(text))
-                    processed_text = re.sub(r'\n{3,}', '\n\n', html_to_md)
-                    value_list[i] = processed_text
-                    # Update both progress bars
-                    inner_bar.update()
-                    outer_bar.update()
+            for i, text in enumerate(value_list):
+                html_to_md = md(str(text))
+                processed_text = re.sub(r'\n{3,}', '\n\n', html_to_md)
+                value_list[i] = processed_text
+                # Update both progress bars
+            outer_bar.update()
     return module_names
 def write_to_files(module_names):
     os.system('clear')
@@ -122,33 +126,28 @@ def write_to_files(module_names):
             folder_path = "/Users/marc.ponce/Documents/Obsidian Vault/Training/Hack the Box/" + key + "/"
             os.makedirs(folder_path, exist_ok=True)
             # Initialize the inner tqdm progress bar for loading files to the module folder
-            with tqdm(total=len(value_list), desc=f"\rLoading files to module {key} folder:") as inner_bar:
-                i = 0
-                # Iterate over the processed list of values
-                for value in value_list:
-                    lines = value.split('\n')
-                    start_index = next((i for i, line in enumerate(lines[1:], start=1) if re.search(r'\w', line)), None)
-                    if start_index is not None:
-                        file = lines[start_index].strip()
-                        file = file.replace("/","-")
+            i = 0
+            # Iterate over the processed list of values
+            for value in value_list:
+                lines = value.split('\n')
+                start_index = next((i for i, line in enumerate(lines[1:], start=1) if re.search(r'\w', line)), None)
+                if start_index is not None:
+                    file = lines[start_index].strip()
+                    file = file.replace("/","-")
                         # Use the first line containing a word as the filename
-                        filename = os.path.join(folder_path + str(i) + ". " + file + ".md")
+                    filename = os.path.join(folder_path + str(i) + ". " + file + ".md")
                         # Increment index for the next file
-                        i += 1
-                        lines.insert(3, "#hacktheboxacademy #theory")
+                    i += 1
+                    lines.insert(3, "#hacktheboxacademy #theory")
                         # Write the value starting from the line with a word to a Markdown file inside the folder
-                        with open(filename, 'w') as f:
-                            f.write('\n'.join(lines[3:]))
-                    inner_bar.update()  # Update the inner progress bar
+                    with open(filename, 'w') as f:
+                        f.write('\n'.join(lines[3:]))
             outer_bar.update()  # Update the outer progress bar
 
 # execution flow
 s = get_session(headers["Cookie"])
 modules, module_names = get_moduleNames(s)
-#print(module_names)
-
 module_links = get_moduleLinks(modules, headers)
-#print(module_links)
-#module_names = get_content(module_links)
-#markdowned = to_markdown(module_names)
-#write_to_files(markdowned)
+module_names = get_content(module_links)
+markdowned = to_markdown(module_names)
+write_to_files(markdowned)
